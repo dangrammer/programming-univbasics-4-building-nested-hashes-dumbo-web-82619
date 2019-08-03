f056f2f227154183644a2a53a37d74d18125eb2d
Dan Romans <danieljromans@gmail.com>
2019-08-03 05:37:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,13 @@
-def base_hash
-  {railroads: {}}
+def base_hash 
+  {
+    railroads: {}
+  }
 end
 
 def monopoly_with_second_tier
-  {railroads: {pieces:}}
+  {
+    railroads: {pieces: 4}
+  }
 end
 
 def monopoly_with_third_tier
